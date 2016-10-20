0734dd4108ead16e10f25c15dc9dff2ec85c6681
岩崎有基 <hurry@MBP.local>
2016-10-20 16:46:35 +0900

    update pic number

--- date_scraping.py
+++ date_scraping.py
@@ -1,20 +1,35 @@
-# coding: utf-8
+# coding:utf-8
 
+import sys
 import csv
 import MeCab
+import re
 
+csv = sys.argv
+tagger = MeCab.Tagger()
 data = []
+chasen_buffer = []
+chasen_list = []
+chasen_data = []
 
-with open("Great_East_Japan_Earthquake.csv", "rb") as f:
-    reader = csv.reader(f)
 
-    for row in reader:
-        data.append(row)
+def change_morpheme(data):
+    chasen = tagger.parse(data[4])
+    chasen_list = chasen.split("\n")
+    # print(chasen_list[0])
 
-tagger = MeCab.Tagger("-Ochasen")
+    for cdata in chasen_list:
+            chasen_data.append(re.split(r'\t|,|', cdata))
+            #print(chasen_data[0])
 
-# リストの状態では数字の羅列になるためn次元のリストだとその階層まで下げる必要がある
-for t in data:
-    # for s in t[3]:
-    chasen = tagger.parse(t[4])
-    print(chasen)
+    print(chasen_data[0][2])
+
+
+def open_csv():
+    with open("Great_East_Japan_Earthquake.csv", "rb") as file:
+        for line in file:
+            data.append(line)
+
+
+open_csv()
+change_morpheme(data)
