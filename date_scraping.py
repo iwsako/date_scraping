39c64d9b32d2ecb8f693ade1be9309ce04a9591d
岩崎有基 <hurry@MBP.local>
2016-10-21 13:44:05 +0900

    update find problem

--- date_scraping.py
+++ date_scraping.py
@@ -25,7 +25,7 @@ def open_csv():
 def make_list():
     # print len(data)
     # 長すぎるため一時省略
-    for sentence in data[:10]:
+    for sentence in data[:7]:
         chasen = tagger.parse(sentence)
         # print(chasen)
         chasen_list = chasen.split("\n")
@@ -33,13 +33,14 @@ def make_list():
         for cdata in chasen_list:
                 chasen_data.append(re.split(r'\t|,|', cdata))
 
-        # print len(chasen_data)
+        print len(chasen_data)
         # 確認用
-        # for textlist in chasen_data:
-        #     print("-------")
-        #     for text in textlist:
-        #         print text
-        #     print("-------")
+        for textlist in chasen_data:
+            if len(textlist) > 1:
+                print("-------")
+                for text in textlist:
+                    print text
+                print("-------")
 
 
 # 日付を抽出
@@ -48,10 +49,12 @@ def pick_date():
     for textlist in chasen_data:
         if len(textlist) > 2:
             # print(textlist[2])
-            if textlist[0] == "年" or textlist[0] == "月" or textlist[0] == "日":
-                print(num_buffer)
+            # 問題点：円なども名詞、接尾、助数詞のためこのやり方では日にち抽出できない
+            if textlist[1] == "名詞" and textlist[2] == "接尾" and textlist[3] == "助数詞":
+                print(num_buffer + textlist[0])
                 # print(textlist[0])
 
+            # 問題点：億などの語句も数という形態素をもっている
             if str(textlist[2]) == "数":
                 num_buffer = textlist[0]
 
