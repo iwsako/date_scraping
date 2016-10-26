e7216ec0e0e3bb64312516313ba9f58ffaa9e66c
岩崎有基 <hurry@MBP.local>
2016-10-26 15:19:05 +0900

    update 15:18

--- date_scraping.py
+++ date_scraping.py
@@ -13,7 +13,7 @@ chasen_list = []
 chasen_data = []
 
 
-def open_csv():
+def openCsv():
     with open("Great_East_Japan_Earthquake.csv", "rb") as file:
         for line in file:
             temp = line.split(",")
@@ -27,10 +27,10 @@ def open_csv():
 
 
 # 形態素にかけてリスト化する関数
-def make_list():
+def makeList():
     # print len(data)
     # 長すぎるため一時省略
-    for sentence in data:
+    for sentence in data[:7]:
         # print sentence
         chasen = tagger.parse(sentence)
         # print(chasen)
@@ -51,24 +51,35 @@ def make_list():
         #         print("-------")
 
 
+# 二重インデントぐらいで書けるようにしておく
+# if文の判定を関数化するときはisかcanをつけて読めるようにしておく
 # 日付を抽出
-def pick_date():
+def pickDate():
     for i in range(len(chasen_data)):
         if len(chasen_data[i]) > 2:
             if chasen_data[i][2] == "数":
-                # 問題点：円なども名詞、接尾、助数詞のためそれを指定できない
-                if chasen_data[i+2][0] == '年間' or chasen_data[i+1][0] == '月間' or chasen_data[i+1][0] == '日間':
+                # 問題点：
+                # if chasen_data[i+1][0] == '年間' or chasen_data[i+1][0] == '月間' or chasen_data[i+1][0] == '日間':
                     # おそらく5日間という期間も5日と表示されている
-                    print chasen_data[i][0] + chasen_data[i+1][0]
-                elif chasen_data[i+1][0] == '年' or chasen_data[i+1][0] == '月' or chasen_data[i+1][0] == '日':
-                    print chasen_data[i][0] + chasen_data[i+1][0]
+                    # print chasen_data[i][0] + chasen_data[i+1][0]
+                    # morpheme_data = chasen_data[i][0] + chasen_data[i+1][0]
+                    # writeCsv(morpheme_data)
+                # elif chasen_data[i+1][0] == '年' or chasen_data[i+1][0] == '月' or chasen_data[i+1][0] == '日':
+                if chasen_data[i+1][0] == '年' or chasen_data[i+1][0] == '月' or chasen_data[i+1][0] == '日':
+                    # print chasen_data[i][0] + chasen_data[i+1][0]
+                    date_data = chasen_data[i][0] + chasen_data[i+1][0]
+                    writeCsv(date_data)
 
 
 # csvに書き込みをする関数
-# def write_csv():
-#     with open("")
+def writeCsv(date_data):
+    # print type(date_data)
+    with open("list.csv", "wb") as f:
+        csvWriter = csv.writer(f)
+        csvWrite.write(date_data)
+    f.close()
 
 
-open_csv()
-make_list()
-pick_date()
+openCsv()
+makeList()
+pickDate()
